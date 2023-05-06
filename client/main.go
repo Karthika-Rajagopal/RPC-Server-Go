8e2bf4b94182bc1512ea0f79598ff02acdaaaa09
Karthika Rajagopal <78952344+Karthika-Rajagopal@users.noreply.github.com>
2023-05-06 13:21:39 +0530

    Update main.go
     RPC methods are implemented on the server-side and registered with rpc.Register, which allows them to be called by clients

--- client/main.go
+++ client/main.go
@@ -15,7 +15,7 @@ func main() {
 	var reply Item
 	var db []Item
 
-	client, err := rpc.DialHTTP("tcp", "localhost:4040")
+	client, err := rpc.DialHTTP("tcp", "localhost:4040")  // creates a client by calling rpc.DialHTTP with the network protocol and address of the server
 
 	if err != nil {
 		log.Fatal("Connection error: ", err)
@@ -25,14 +25,14 @@ func main() {
 	b := Item{"Second", "A second item"}
 	c := Item{"Third", "A third item"}
 
-	client.Call("API.AddItem", a, &reply)
+	client.Call("API.AddItem", a, &reply)  //calls the server's AddItem method three times with different items as arguments and stores the response in reply
 	client.Call("API.AddItem", b, &reply)
 	client.Call("API.AddItem", c, &reply)
-	client.Call("API.GetDB", "", &db)
+	client.Call("API.GetDB", "", &db)//then calls the server's GetDB method with an empty string as argument to retrieve the current state of the database, and stores the result in the db variable
 
 	fmt.Println("Database: ", db)
 
-	client.Call("API.EditItem", Item{"Second", "A new second item"}, &reply)
+	client.Call("API.EditItem", Item{"Second", "A new second item"}, &reply) // updating  item with new body
 
 	client.Call("API.DeleteItem", c, &reply)
 	client.Call("API.GetDB", "", &db)
